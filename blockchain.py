cfd3b34f57d338009624b8c5a2c10f41d22bc347
frankieyeung <113824377+frankieyeung@users.noreply.github.com>
2023-02-12 22:31:57 +0800

    Create create_block method

--- blockchain.py
+++ blockchain.py
@@ -9,10 +9,13 @@ class Blockchain:
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0') #genesis block
 
-q   def create_block(self, proof, previous_hash):
+    def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain) + 1,
                  'timestamp': str(datetime.datetime.now()),
                  'proof': proof,
                  'previous_hash': previous_hash}
         self.chain.append(block)
         return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
